f1d7888a9b00b64d6b83e9c798f67b1aa4beffda
Allister Isaiah Harvey <isaiahharvey1998@outlook.com>
2023-04-21 08:27:22 -0400

    Added error messages and ability to set relative paths

--- WinWallpaper/WinWallpaper.cpp
+++ WinWallpaper/WinWallpaper.cpp
@@ -6,122 +6,165 @@
 #include "shobjidl_core.h"
 #include <comdef.h>
 #include <iostream>
+#include <filesystem>
 #include <string>
 #include <vector>
 
 struct Monitor {
-  LPWSTR monitorIndex;
-  LPWSTR wallpaper;
+	LPWSTR monitorIndex;
+	LPWSTR wallpaper;
 
-  Monitor(LPWSTR monitorIndex, LPWSTR wallpaper) {
-    this->monitorIndex = monitorIndex;
-    this->wallpaper = wallpaper;
-  }
+	Monitor(LPWSTR monitorIndex, LPWSTR wallpaper) {
+		this->monitorIndex = monitorIndex;
+		this->wallpaper = wallpaper;
+	}
+
+	~Monitor() {
+
+	}
 };
 
 class MyDesktopWallpaper {
-  IDesktopWallpaper *pDesk;
+	IDesktopWallpaper* pDesk = nullptr;
 
 public:
-  MyDesktopWallpaper() {
-    CoInitialize(NULL);
-    CoCreateInstance(__uuidof(DesktopWallpaper), NULL, CLSCTX_LOCAL_SERVER,
-                     IID_IDesktopWallpaper, (void **)&pDesk);
-  }
+	MyDesktopWallpaper() {
+		HRESULT hr = CoInitialize(NULL);
+		if (FAILED(hr)) {
+			std::cout << "Failed to initialize COM" << std::endl;
+			return;
+		}
 
-  ~MyDesktopWallpaper() {
-    pDesk->Release();
-    CoFreeUnusedLibraries();
-    CoUninitialize();
-  }
+		hr = CoCreateInstance(__uuidof(DesktopWallpaper), NULL, CLSCTX_LOCAL_SERVER,
+			IID_IDesktopWallpaper, (void**)&pDesk);
 
-  std::vector<Monitor> getMonitors() {
-    UINT monitorCount = 0;
-    pDesk->GetMonitorDevicePathCount(&monitorCount);
+		if (FAILED(hr)) {
+			std::cout << "Failed to create COM instance" << std::endl;
+			return;
+		}
+	}
 
-    std::vector<Monitor> monitors;
+	~MyDesktopWallpaper() {
+		pDesk->Release();
+		CoFreeUnusedLibraries();
+		CoUninitialize();
+	}
 
-    for (UINT index = 0; index < monitorCount; index++) {
-      LPWSTR monitorId = NULL;
-      pDesk->GetMonitorDevicePathAt(index, &monitorId);
+	std::vector<Monitor> getMonitors() {
+		UINT monitorCount = 0;
+		pDesk->GetMonitorDevicePathCount(&monitorCount);
 
-      LPWSTR wallpaper = NULL;
-      pDesk->GetWallpaper(monitorId, &wallpaper);
+		std::vector<Monitor> monitors;
 
-      monitors.push_back(Monitor(monitorId, wallpaper));
-    }
+		for (UINT index = 0; index < monitorCount; index++) {
+			LPWSTR monitorId = NULL;
+			pDesk->GetMonitorDevicePathAt(index, &monitorId);
 
-    return monitors;
-  }
+			LPWSTR wallpaper = NULL;
+			pDesk->GetWallpaper(monitorId, &wallpaper);
 
-  void SetWallpaper(LPCWSTR fullPath, Monitor monitor,
-                    DESKTOP_WALLPAPER_POSITION position) {
+			monitors.push_back(Monitor(monitorId, wallpaper));
+		}
 
-    pDesk->SetWallpaper(LPCWSTR(monitor.monitorIndex), fullPath);
-    pDesk->SetPosition(position);
-  }
+		return monitors;
+	}
 
-  LPWSTR GetWallpaper(Monitor monitor) {
-    LPWSTR wallpaper = nullptr;
+	void SetWallpaper(LPCWSTR fullPath, Monitor monitor,
+		DESKTOP_WALLPAPER_POSITION position) {
 
-    pDesk->GetWallpaper(monitor.monitorIndex, &wallpaper);
+		HRESULT hr = pDesk->SetWallpaper(LPCWSTR(monitor.monitorIndex), fullPath);
+		if (FAILED(hr)) {
+			std::cout << "Failed to set the desktop wallpaper" << std::endl;
+			return;
+		}
+		hr = pDesk->SetPosition(position);
+		if (FAILED(hr)) {
+			std::cout << "Failed to set the desktop wallpaper position" << std::endl;
+			return;
+		}
+	}
 
-    return wallpaper;
-  }
+	LPWSTR GetWallpaper(Monitor monitor) {
+		LPWSTR wallpaper = nullptr;
+
+		HRESULT hr = pDesk->GetWallpaper(monitor.monitorIndex, &wallpaper);
+		
+		if (FAILED(hr)) {
+			std::cout << "Failed to get the desktop wallpaper path" << std::endl;
+		}
+
+		return wallpaper;
+	}
 };
 
-int main(int argc, char *argv[]) {
-  LPCWSTR path = NULL;
-  auto wallpaper = MyDesktopWallpaper();
-  auto monitors = wallpaper.getMonitors();
+int main(int argc, char* argv[]) {
+	LPCWSTR path = NULL;
+	auto wallpaper = MyDesktopWallpaper();
+	auto monitors = wallpaper.getMonitors();
 
-  if (argc > 1) {
-    if (std::string(argv[1]) == "get") {
-      auto monitor = monitors.at(0);
-      if (argv[2] != nullptr) {
-        int index;
-        sscanf_s((argv[2]), "%d", &index);
-        monitor = monitors.at(index);
-      }
+	if (argc > 1) {
+		if (std::string(argv[1]) == "get") {
+			auto monitor = monitors.at(0);
+			if (argv[2] != nullptr) {
+				int index;
+				sscanf_s((argv[2]), "%d", &index);
+				monitor = monitors.at(index);
+			}
 
-      std::wcout << wallpaper.GetWallpaper(monitor) << std::endl;
+			std::wcout << wallpaper.GetWallpaper(monitor) << std::endl;
 
-    } else if (std::string(argv[1]) == "set") {
-      auto monitor = monitors.at(0);
-      auto position = DESKTOP_WALLPAPER_POSITION::DWPOS_SPAN;
+		}
+		else if (std::string(argv[1]) == "set") {
+			auto monitor = monitors.at(0);
+			auto position = DESKTOP_WALLPAPER_POSITION::DWPOS_SPAN;
 
-      if (argv[2] != nullptr) {
-        int size_needed = MultiByteToWideChar(CP_UTF8, 0, argv[2], -1, NULL, 0);
-        path = new WCHAR[size_needed];
-        MultiByteToWideChar(CP_UTF8, 0, argv[2], -1, (LPWSTR)path, size_needed);
+			if (argv[2] != nullptr) {
+				auto imagePath = std::filesystem::path(argv[2]);
+				if (!std::filesystem::exists(imagePath)) {
+					std::cout << "The specified path does not exist" << std::endl;
+					return 1;
+				}
 
-        if (argv[3] != nullptr) {
-          if (std::string(argv[3]).find("--scale=") != std::string::npos) {
-            auto scaleAsString = std::string(argv[3]).replace(0, 8, "");
-            if (scaleAsString == "center") {
-              position = DWPOS_CENTER;
-            } else if (scaleAsString == "stretch") {
-              position = DWPOS_STRETCH;
-            } else if (scaleAsString == "tile") {
-              position = DWPOS_TILE;
-            } else if (scaleAsString == "span") {
-              position = DWPOS_SPAN;
-            } else if (scaleAsString == "fit") {
-              position = DWPOS_FIT;
-            } else if (scaleAsString == "fill") {
-              position = DWPOS_FILL;
-            }
-          }
-        }
+				wchar_t fullPath[MAX_PATH];
+				if (GetFullPathName(imagePath.c_str(), MAX_PATH, fullPath, nullptr) == 0)
+				{
+					std::cout << "Could not get the full path to file" << std::endl;
+					return 1;
+				}
 
-        wallpaper.SetWallpaper(path, monitor, position);
-      } else {
-        std::cout << "Please provide a path to an image" << std::endl;
-      }
-    }
-  }
+				if (argv[3] != nullptr) {
+					if (std::string(argv[3]).find("--scale=") != std::string::npos) {
+						auto scaleAsString = std::string(argv[3]).replace(0, 8, "");
+						if (scaleAsString == "center") {
+							position = DWPOS_CENTER;
+						}
+						else if (scaleAsString == "stretch") {
+							position = DWPOS_STRETCH;
+						}
+						else if (scaleAsString == "tile") {
+							position = DWPOS_TILE;
+						}
+						else if (scaleAsString == "span") {
+							position = DWPOS_SPAN;
+						}
+						else if (scaleAsString == "fit") {
+							position = DWPOS_FIT;
+						}
+						else if (scaleAsString == "fill") {
+							position = DWPOS_FILL;
+						}
+					}
+				}
 
-  delete[] path;
+				wallpaper.SetWallpaper(fullPath, monitor, position);
+			}
+			else {
+				std::cout << "Please provide a path to an image" << std::endl;
+			}
+		}
+	}
+
+	delete[] path;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
